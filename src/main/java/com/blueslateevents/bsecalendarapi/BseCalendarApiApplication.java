24a35febacacc45b45881275ab15af89b5ed8bd0
Mohammed Shethwala <shethwala.mohammed@gmail.com>
2022-11-02 21:54:38 -0500

    Setting up Springboot & Maven directory structure

---
+++ src/main/java/com/blueslateevents/bsecalendarapi/BseCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.blueslateevents.bsecalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BseCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BseCalendarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blueslateevents/bsecalendarapi/BseCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueslateevents.bsecalendarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BseCalendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
